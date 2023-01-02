21b9850a3538bc6933b553d8974a7f7e51b17787
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-02 13:47:56 -0300

    fixing captcha

--- app/models/concerns/user_agreements.rb
+++ app/models/concerns/user_agreements.rb
@@ -4,7 +4,7 @@ module UserAgreements
   included do
     # Accept the terms of service on registration
     attribute :terms_of_service
-    validates :terms_of_service, presence: true, acceptance: true, on: %i[create invitation_accepted]
+    validates :terms_of_service, presence: false, acceptance: true, on: %i[create invitation_accepted]
 
     after_validation :accept_terms, on: %i[create invitation_accepted]
     after_validation :accept_privacy, on: %i[create invitation_accepted]
