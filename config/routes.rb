8f70d9d56d589dbd55cac3c1a838ffa473f2e220
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-09 16:56:15 -0300

    adding logic behind field player count

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :histories
   resources :configs
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :futengages
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
