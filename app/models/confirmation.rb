f9918ce44e8d53c9496e85c031f3b840b0cd173f
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-14 23:14:29 -0300

    creating correct model

--- app/models/confirmation.rb
+++ app/models/confirmation.rb
@@ -1,2 +1,30 @@
+# == Schema Information
+#
+# Table name: confirmations
+#
+#  id           :bigint           not null, primary key
+#  confirmed    :boolean          default(FALSE), not null
+#  confirmed_at :datetime         not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  match_id     :bigint           not null
+#  member_id    :bigint           not null
+#
+# Indexes
+#
+#  index_confirmations_on_match_id   (match_id)
+#  index_confirmations_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (match_id => matches.id)
+#  fk_rails_...  (member_id => members.id)
+#
 class Confirmation < ApplicationRecord
+  belongs_to :member
+  belongs_to :match
+
+  has_one :team, through: :member
+  has_one :user, through: :member
+  has_one :place, through: :match
 end
