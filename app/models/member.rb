8a722f9b7d43b93d4b93a6b03cfe3c4988c04cc1
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-14 23:14:36 -0300

    creating correct model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  team_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_members_on_team_id  (team_id)
+#  index_members_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (team_id => teams.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Member < ApplicationRecord
+  belongs_to :team
+  belongs_to :user
+
+  has_many :confirmations, dependent: :destroy
 end
