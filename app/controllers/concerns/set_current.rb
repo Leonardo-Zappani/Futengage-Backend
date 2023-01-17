9fce42ee5d524221d61082ef8c8d2679f2d8a112
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-17 16:57:30 -0300

    improving set_current, improving querys

--- app/controllers/concerns/set_current.rb
+++ app/controllers/concerns/set_current.rb
@@ -2,32 +2,58 @@ module SetCurrent
   extend ActiveSupport::Concern
 
   def current_teams 
-    @list_teams = Team.joins(:members).where("members.user_id = ?", current_user.id).all
+    if @current_match.present?
+      @list_teams = Team.joins(:members).where("members.user_id = ?", current_user.id).all
+      return @list_teams
+    end
+  end
+
+  def current_team
+    if @current_match.present?
+      @current_team = @current_match.team
+      return @current_team
+    end
   end
 
   def current_match
-    @current_match = current_user.teams.joins(:matches).where("matches.scheduled_at > ?", Time.now).order("matches.scheduled_at ASC").first.matches.where("matches.scheduled_at > ?", Time.now).order("matches.scheduled_at ASC").first
+    if current_user.teams.present? && current_user.teams.joins(:matches).any?
+      @current_match = current_user.teams.joins(:matches).where("matches.scheduled_at > ?", Time.now).order("matches.scheduled_at ASC").first.matches.where("matches.scheduled_at > ?", Time.now).order("matches.scheduled_at ASC").first  
+    end
   end
 
   def current_member
-    @current_member = @current_match.team.members.where(user_id: current_user.id)
+    if @current_match.present?
+      @current_member = @current_match.team.members.find_by(user_id: current_user.id)
+    else
+      nil
+    end
   end
 
   def list_match
-    @list_match = @all_matches
+    if @current_match.present?
+      @all_matches = @current_match.team.matches
+      return @all_matches
+    end
   end
-  
+
   def current_confirmation
-    @current_confirmation = Confirmation.joins(match: [team: :members]).where("members.user_id = ?", params[:user_id]).order("matches.scheduled_at ASC").first
+    if @current_match.present?  
+      @current_confirmation = @current_match.confirmations.find_by(member_id: current_member)
+      return @current_confirmation
+    end
   end
 
   def current_peding_confirmation
-    @current_peding_confirmation = current_match.confirmations.first
+    if @current_match.present?
+      @current_peding_confirmation = @current_match.confirmations.find_by(member_id: current_member)
+      return @current_peding_confirmation
+    end
   end
 
   def current_player_count
-    @current_player_count = @current_match.confirmations.count
-    @current_confirmed_player_count = @current_match.confirmations.where(confirmed: true).count
+    if @current_match.present?
+      @current_player_count = @current_match.team.members.count
+      @current_confirmed_count = @current_match.confirmations.where("confirmed = ?", true).count
+    end
   end
-
-end  
+end
