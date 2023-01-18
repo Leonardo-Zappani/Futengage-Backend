1ceb2343007cc5a3c86727ef413909666de37717
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-18 17:23:40 -0300

    fixing migration

--- db/migrate/9_create_matches.rb
+++ db/migrate/9_create_matches.rb
@@ -5,10 +5,10 @@ class CreateMatches < ActiveRecord::Migration[7.0]
       t.references :place,                null: false, foreign_key: true
       t.references :owner,                null: false, foreign_key: { to_table: :users }
 
-      t.string :team_one_name,             null: false
-      t.string :team_two_name,             null: false
-      t.string :team_one_score,            null: false
-      t.string :team_two_score,            null: false
+      t.string :team_one_name,             null: false, default: "team one"
+      t.string :team_two_name,             null: false, default: "team two"
+      t.string :team_one_score,            null: false, default: "0"
+      t.string :team_two_score,            null: false, default: "0"
       
       t.datetime :scheduled_at
       t.datetime :confirmed_at
