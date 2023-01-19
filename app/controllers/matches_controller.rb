bef381b3cacb9d869402ea4c5d86f8f96afae2c2
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-19 15:31:37 -0300

    fix

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -28,21 +28,25 @@ class MatchesController < ApplicationController
 
   # POST /matches or /matches.json
   def create
-    @match = current_account.matches.new(match_params)
-
-    respond_to do |format|
-      if @match.save
-        notice = t('.success')
-        format.html { redirect_to matches_url, notice: notice }
-        format.json { render :show, status: :created, location: @match }
-        format.turbo_stream { flash.now.notice = notice }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @match.errors, status: :unprocessable_entity }
+    @team = Team.find(params[:match][:team_id])
+    @place = Place.find(params[:match][:place_id])
+    puts @place 
+    puts @team
+    
+    @match = @team.matches.new(scheduled_at: params[:match][:scheduled_at], place: @place, owner_id: params[:match][:owner_id], team_one_name: params[:match][:team_one_name], team_two_name: params[:match][:team_two_name], team_one_score: params[:match][:team_one_score], team_two_score: params[:match][:team_two_score], confirmed_at: params[:match][:confirmed_at])
+      respond_to do |format|
+        if @match.save
+          notice = t('.success')
+          format.html { redirect_to matches_url, notice: notice }
+          format.json { render :show, status: :created, location: @match }
+          format.turbo_stream { flash.now.notice = notice }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @match.errors, status: :unprocessable_entity }
       end
     end
   end
-
+  
   # PATCH/PUT /matches/1 or /matches/1.json
   def update
     respond_to do |format|
@@ -78,6 +82,6 @@ class MatchesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def match_params
-    params.require(:match).permit(:user_id,:name , :time, :day, :place, :scoreboard, :team_name_2, :team_name_1, :scheduled_at)
+    params.require(:match).permit(:scheduled_at, :team_id, :place_id, :owner_id, :team_one_name, :team_two_name, :team_one_score, :team_two_score, :confirmed_at)
   end
 end
