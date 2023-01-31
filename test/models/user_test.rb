68bc4a16abae8f67f65e140e7bcaa716837fd38a
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-01-31 17:43:13 -0300

    tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,6 +18,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  role                   :integer          default("goleiro")
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  invited_by_id          :integer
