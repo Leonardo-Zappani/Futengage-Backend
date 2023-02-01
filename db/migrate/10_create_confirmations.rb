fce2063ea572f7b66821405132bd413299dd16ea
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-02-01 17:23:36 -0300

    improving confirmations

--- db/migrate/10_create_confirmations.rb
+++ db/migrate/10_create_confirmations.rb
@@ -3,6 +3,7 @@ class CreateConfirmations < ActiveRecord::Migration[7.0]
     create_table :confirmations do |t|
       t.references :member,              null: false, foreign_key: true
       t.references :match,               null: false, foreign_key: true
+      t.references :user,                null: false, foreign_key: true
 
       t.integer :position
       t.integer :team_number
