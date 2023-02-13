02896953b5346f8607173c55288a95866bd96caa
Leonardo Fernandes Zappani <102327012+LeonardoUTF@users.noreply.github.com>
2023-02-13 09:27:42 -0300

    fixing web scale

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -67,7 +67,7 @@ module ApplicationHelper
 
   def viewport_meta_tag(content: 'width=device-width, initial-scale=1',
                         turbo_native: 'maximum-scale=1.0, user-scalable=0')
-    full_content = [content, (turbo_native if turbo_native_app?)].compact.join(', ')
+    full_content = [content]
     tag.meta name: 'viewport', content: full_content
   end
 
