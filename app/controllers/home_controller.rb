18f9fc8eead028f5c0139c78e6f323e29fcfea06
Leonardo Fernandes Zappani <leonardozappani@alunos.utfpr.edu.br>
2023-08-12 21:39:53 -0300

    refactoring controller home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @next_match = current_user.next_match
-    @next_confirmation = current_user.next_confirmation
+    @next_match = current_user&.next_match
+    @next_confirmation = current_user&.next_confirmation
 
-    render json: { next_match: @next_match, next_confirmation: @next_confirmation }
+    render json: { next_match: @next_match, next_confirmation: @next_confirmation } if current_user.present?
   end
 
 
